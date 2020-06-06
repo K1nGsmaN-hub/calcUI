b7189d6584ddc0434c18f3d0435095c661aec63f
K1nGsmaN-hub <15162342h@gmail.com>
2020-06-07 07:08:32 +0800

    calcUI v1.1

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# calcUI
+# calcUI - Jun7
 ## CalcUI v1.0
 ![](https://i.ibb.co/LSP8Y6P/calc-UI-v1-0.png)
 ### What can it do:
 * Multiply by, Divide by, Add and Subtract
 * automatic prioritizes
 to be continued...
+## CalcUI v1.1 - Jun7
+![](https://i.ibb.co/TYKKXMk/calc-UI-v1-1.png)
+### Added:
+* Backspace button (script/main.js::5 and ::25)
+* Changed icon for Multiply button and Divide button
+* Included "FontAwesome"
+* Changed font to Montserrat
--- script/main.js
+++ script/main.js
@@ -1,11 +1,18 @@
 const buttonNumbers = document.querySelectorAll('.number_item'), // All button without 'AC' and '='
       inputMonitor = document.querySelector('#display'), // Calc monitor
       buttonClean = document.querySelector('.clean-button'), // 'AC' button
-      buttonEquals = document.querySelector('.equals-button'); // '=' button 
+      buttonEquals = document.querySelector('.equals-button'), // '=' button 
+      buttonBackspace = document.querySelector('.backspace-button'); // backsapce button
 
 for (buttonNumber of buttonNumbers) {
     buttonNumber.addEventListener('click', function () {
-        inputMonitor.value = inputMonitor.value + this.value; // Display number to calcMonitor
+        if (this.value == '÷') {
+            inputMonitor.value = inputMonitor.value + '/';
+        } else if (this.value == '×') {
+            inputMonitor.value = inputMonitor.value + '*';
+        }else {
+            inputMonitor.value = inputMonitor.value + this.value; // Display a number to the calcMonitor
+        }
     })
 }
 
@@ -15,4 +22,6 @@ buttonClean.addEventListener('click', function () {
 buttonEquals.addEventListener('click', function () {
     inputMonitor.value = eval(inputMonitor.value); // Computes expression
 })
-
+buttonBackspace.addEventListener('click', function() {
+    inputMonitor.value = inputMonitor.value.substring(0, inputMonitor.value.length-1); // Delete the last symbol
+})
