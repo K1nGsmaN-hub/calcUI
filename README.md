1196d9da7b738c6144475a53d2c50dae145f0da0
K1nGsmaN-hub <15162342h@gmail.com>
2020-06-11 03:51:58 +0800

    calcUI v1.2

--- README.md
+++ README.md
@@ -8,7 +8,14 @@ to be continued...
 ## CalcUI v1.1 - Jun7
 ![](https://i.ibb.co/TYKKXMk/calc-UI-v1-1.png)
 ### Added:
-* Backspace button (script/main.js::5 and ::25)
+* Backspace button
 * Changed icon for Multiply button and Divide button
 * Included "FontAwesome"
 * Changed font to Montserrat
+## CalcUI v1.2 - Jun11
+![](https://i.ibb.co/Syw0dv7/calc-UI-v1-2.png)
+### Added:
+* Second monitor (first - input, second - output)
+* Horizontal orentation
+* Rotate button
+* Unworkable buttons for horizontal orentation
--- script/main.js
+++ script/main.js
@@ -1,8 +1,13 @@
+//vertical calc consts
 const buttonNumbers = document.querySelectorAll('.number_item'), // All button without 'AC' and '='
-      inputMonitor = document.querySelector('#display'), // Calc monitor
+      inputMonitor = document.querySelector('#input-monitor'), // Calc monitor
+      resultMonitor = document.querySelector('#result-monitor'),
       buttonClean = document.querySelector('.clean-button'), // 'AC' button
       buttonEquals = document.querySelector('.equals-button'), // '=' button 
       buttonBackspace = document.querySelector('.backspace-button'); // backsapce button
+
+// horizontal calc consts
+const rotateButton = document.querySelector('.rotate-button');
 
 for (buttonNumber of buttonNumbers) {
     buttonNumber.addEventListener('click', function () {
@@ -17,11 +22,19 @@ for (buttonNumber of buttonNumbers) {
 }
 
 buttonClean.addEventListener('click', function () {
+    resultMonitor.value = '';
     inputMonitor.value = ''; // clean calcMonitor
 })
 buttonEquals.addEventListener('click', function () {
-    inputMonitor.value = eval(inputMonitor.value); // Computes expression
+    resultMonitor.value = eval(inputMonitor.value); // Computes expression
+    inputMonitor.value = inputMonitor.value + this.value;
 })
 buttonBackspace.addEventListener('click', function() {
     inputMonitor.value = inputMonitor.value.substring(0, inputMonitor.value.length-1); // Delete the last symbol
+})
+
+
+rotateButton.addEventListener('click', function () {
+    document.querySelector('.content').classList.toggle('active-horizontal');
+    document.querySelector('.content__formulas').classList.toggle('visible');
 })
